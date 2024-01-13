80b2f9bdad9785221b473bfc09a326558079f3a3
DeemTheDev <136303631+DeemTheDev@users.noreply.github.com>
2024-01-13 03:36:58 +0200

    almost done

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
-// Function to display the modal
-function showPopup() {
-    var modal = document.getElementById('myModal');
+function showPopup(modalId) {
+    var modal = document.getElementById(modalId);
     modal.style.display = 'block';
 }
 
-// Function to close the modal
-function closePopup() {
-    var modal = document.getElementById('myModal');
+function closePopup(modalId) {
+    var modal = document.getElementById(modalId);
     modal.style.display = 'none';
 }
 
-// Close the modal if the user clicks outside of it
 window.onclick = function(event) {
-    var modal = document.getElementById('myModal');
-    if (event.target === modal) {
-        modal.style.display = 'none';
+    var modals = document.getElementsByClassName('modal');
+    for (var i = 0; i < modals.length; i++) {
+        if (event.target === modals[i]) {
+            modals[i].style.display = 'none';
+        }
     }
 };
